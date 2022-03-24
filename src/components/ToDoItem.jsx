ee3dc4133b5aa4a77a76544d1deebaf7cd7e66b8
Harsha CR <pricenoob008@gmail.com>
2022-03-24 12:39:50 +0530

    Commit

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ToDoItem(props) {
+  return (
+    <div>
+      <li onClick={() => props.onChecked(props.id)}>{props.text}</li>
+    </div>
+  );
+}
+export default ToDoItem;
